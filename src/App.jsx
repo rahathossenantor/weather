11542eefe10e54446792ce9b816a1fbd68c4e4f0
rahathossenantor <md.rahathossenantor@gmail.com>
2025-11-04 16:37:38 +0600

    WeaterBoard section is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./sections/header/Header";
+import WeatherBoard from "./sections/weather/WeatherBoard";
 
 export default function App() {
     return (
         <>
             <Header />
+            <main>
+                <section>
+                    <WeatherBoard />
+                </section>
+            </main>
         </>
     );
 }
